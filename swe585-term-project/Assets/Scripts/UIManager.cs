fc6219594909839f1d7685b9315fd8804651f45f
Rhonur <onur.demirkale@outlook.com>
2022-06-04 19:18:40 +0300

    update

--- swe585-term-project/Assets/Scripts/UIManager.cs
+++ swe585-term-project/Assets/Scripts/UIManager.cs
@@ -10,9 +10,75 @@ public class UIManager : MonoBehaviour
     public InputField inputField;
     private float inputValue = 0;
 
+    public InputField rotationOverLifetimeField;
+    private float rotationOverLifetimeValue = 0;
+
+    public InputField rotationBySpeedField;
+    private float rotationBySpeedValue = 0;
+
+    public InputField angleInput;
+    private float angleInputValue = 0;
+
     private bool collisionToggle = false;
     private bool triggerToggle = false;
     private bool prewarmToggle = false;
+    private bool shadowsToggle = false;
+
+    public void ToggleShadows()
+    {
+        var renderer = particle.renderer;
+
+        // if(shadowsToggle)
+        // {
+        //     renderer.shadowCastingMode = renderer.shadowCastingMode.Off;
+        //     shadowsToggle = false; 
+        // } 
+        // else 
+        // {
+        //     renderer.shadowCastingMode = renderer.shadowCastingMode.On;
+        //     shadowsToggle = true;
+        // }
+    }
+
+    public void SetRotationOverLifetime()
+    {
+        var inputText = rotationOverLifetimeField.text.ToString();
+        rotationOverLifetimeValue = float.Parse(inputText);
+
+        var rotationOverLifetime = particle.rotationOverLifetime;
+
+        rotationOverLifetime.zMultiplier =  (Mathf.PI * rotationOverLifetimeValue);
+    }
+
+    public void SetRotationBySpeed()
+    {
+        var inputText = rotationBySpeedField.text.ToString();
+        rotationBySpeedValue = float.Parse(inputText);
+
+        var rotationBySpeed = particle.rotationBySpeed;
+
+        rotationBySpeed.zMultiplier = rotationBySpeedValue;
+    }
+
+    public void SetAngle()
+    {
+        var angleInputText = angleInput.text.ToString();
+        angleInputValue = float.Parse(angleInputText);
+
+        if (angleInputValue < 0)
+        {
+            angleInputValue = 0;
+        }
+
+        if (angleInputValue > 90)
+        {
+            angleInputValue = 90;
+        }
+
+        var shape = particle.shape;
+
+        shape.angle = angleInputValue;
+    }
 
     // Prewarm allows creation of particles beforehand.
     public void EnablePrewarm()
