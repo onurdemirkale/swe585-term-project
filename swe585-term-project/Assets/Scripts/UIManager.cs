ebd820c5e479a0464fa7d05e4237e0b89762a846
Rhonur <onur.demirkale@outlook.com>
2022-06-04 14:54:35 +0300

    project progress

--- swe585-term-project/Assets/Scripts/UIManager.cs
+++ swe585-term-project/Assets/Scripts/UIManager.cs
@@ -8,16 +8,72 @@ public class UIManager : MonoBehaviour
     public ParticleSystem particle;
 
     public InputField inputField;
-    private float inputValue;
+    private float inputValue = 0;
 
-    public void BeginCollision()
-    {
-        var coll = particle.collision;
-        coll.enabled = true;
+    private bool collisionToggle = false;
+    private bool triggerToggle = false;
+    private bool prewarmToggle = false;
+
+    // Prewarm allows creation of particles beforehand.
+    public void EnablePrewarm()
+    {   
+        var main = particle.main;
+
+        if(prewarmToggle)
+        {
+            main.prewarm = false;
+            prewarmToggle = false;
+        } 
+        else 
+        {
+            main.prewarm = true;
+            prewarmToggle = true;
+        }
     }
 
-    public void ChangeEmissionRate()
+    // Enable collision between the particles and the floor cube.
+    public void ToggleCollision()
+    {   
+        var collision = particle.collision;
+
+        if(collisionToggle)
+        {
+            collision.enabled = false;
+            collisionToggle = false;
+        } 
+        else 
+        {
+            collision.enabled = true;
+            collisionToggle = true;
+        }
+    }
+
+    // Toggle trigger between the particles and the floor cube.
+    // Once particles hit the floor when trigger is on, they 
+    // are removed.
+    public void ToggleTrigger()
     {
+        var trigger = particle.trigger;
+
+        if(triggerToggle)
+        {
+            trigger.enabled = false;
+            triggerToggle = false;
+        }
+        else
+        {
+            trigger.enabled = true;
+            triggerToggle = true;
+        }
+
+        
+    }
+
+    // Change the emission rate of the particles.
+    public void SetEmissionRate()
+    {   
+        var inputText = inputField.text.ToString();
+        inputValue = float.Parse(inputText);
         particle.emissionRate = inputValue;
     }
 }
